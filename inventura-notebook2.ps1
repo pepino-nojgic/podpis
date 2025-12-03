d6453ab4e2a95a194607091790269b43df8ab028
pepino-nojgic <josef.neugebauer@gmail.com>
2025-12-03 13:13:19 +0100

    Enhance GUI and device info collection in script
    Updated the GUI layout and improved the handling of device information collection.

--- inventura-notebook2.ps1
+++ inventura-notebook2.ps1
@@ -26,14 +26,15 @@ Add-Type -AssemblyName System.Drawing
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
 $form = New-Object System.Windows.Forms.Form
-$form.Text = "Inventura notebooku Media:list"
-$form.Size = New-Object System.Drawing.Size(430, 330)
+$form.Text = "Inventura notebooku Medialist"
+$form.Size = New-Object System.Drawing.Size(520, 380)   # vetsi okno
 $form.StartPosition = "CenterScreen"
 
 # Label s informaci o zarizeni
 $lblDevice = New-Object System.Windows.Forms.Label
 $lblDevice.Text = "Zarizeni: $manufacturer $model (SN: $serial)"
 $lblDevice.AutoSize = $true
+$lblDevice.MaximumSize = New-Object System.Drawing.Size(480, 0)  # zalamovani radku
 $lblDevice.Location = New-Object System.Drawing.Point(10, 10)
 $form.Controls.Add($lblDevice)
 
@@ -41,51 +42,52 @@ $form.Controls.Add($lblDevice)
 $lblName = New-Object System.Windows.Forms.Label
 $lblName.Text = "Jmeno a prijmeni:"
 $lblName.AutoSize = $true
-$lblName.Location = New-Object System.Drawing.Point(10, 40)
+$lblName.Location = New-Object System.Drawing.Point(10, 50)
 $form.Controls.Add($lblName)
 
 $txtName = New-Object System.Windows.Forms.TextBox
-$txtName.Size = New-Object System.Drawing.Size(380, 20)
-$txtName.Location = New-Object System.Drawing.Point(10, 60)
+$txtName.Size = New-Object System.Drawing.Size(480, 20)
+$txtName.Location = New-Object System.Drawing.Point(10, 70)
 $form.Controls.Add($txtName)
 
 # Typ zarizeni (firemni / osobni)
 $lblType = New-Object System.Windows.Forms.Label
 $lblType.Text = "Typ zarizeni:"
 $lblType.AutoSize = $true
-$lblType.Location = New-Object System.Drawing.Point(10, 95)
+$lblType.Location = New-Object System.Drawing.Point(10, 105)
 $form.Controls.Add($lblType)
 
 $rbFiremni = New-Object System.Windows.Forms.RadioButton
 $rbFiremni.Text = "Firemni"
-$rbFiremni.Location = New-Object System.Drawing.Point(20, 115)
+$rbFiremni.Location = New-Object System.Drawing.Point(20, 125)
 $rbFiremni.Checked = $true
 $form.Controls.Add($rbFiremni)
 
 $rbOsobni = New-Object System.Windows.Forms.RadioButton
 $rbOsobni.Text = "Osobni"
-$rbOsobni.Location = New-Object System.Drawing.Point(120, 115)
+$rbOsobni.Location = New-Object System.Drawing.Point(120, 125)
 $form.Controls.Add($rbOsobni)
 
 # Poznamka
 $lblNotes = New-Object System.Windows.Forms.Label
 $lblNotes.Text = "Poznamka (napr. rozbite tlacitko, nepouzivany atd.):"
 $lblNotes.AutoSize = $true
-$lblNotes.Location = New-Object System.Drawing.Point(10, 145)
+$lblNotes.MaximumSize = New-Object System.Drawing.Size(480, 0)
+$lblNotes.Location = New-Object System.Drawing.Point(10, 155)
 $form.Controls.Add($lblNotes)
 
 $txtNotes = New-Object System.Windows.Forms.TextBox
 $txtNotes.Multiline = $true
 $txtNotes.ScrollBars = "Vertical"
-$txtNotes.Size = New-Object System.Drawing.Size(380, 90)
-$txtNotes.Location = New-Object System.Drawing.Point(10, 165)
+$txtNotes.Size = New-Object System.Drawing.Size(480, 140)
+$txtNotes.Location = New-Object System.Drawing.Point(10, 175)
 $form.Controls.Add($txtNotes)
 
 # Tlacitka OK / Zrusit
 $btnOK = New-Object System.Windows.Forms.Button
 $btnOK.Text = "Odeslat"
-$btnOK.Size = New-Object System.Drawing.Size(80, 30)
-$btnOK.Location = New-Object System.Drawing.Point(230, 265)
+$btnOK.Size = New-Object System.Drawing.Size(90, 30)
+$btnOK.Location = New-Object System.Drawing.Point(300, 325)
 $btnOK.Add_Click({
     $form.Tag = "OK"
     $form.Close()
@@ -94,16 +96,16 @@ $form.Controls.Add($btnOK)
 
 $btnCancel = New-Object System.Windows.Forms.Button
 $btnCancel.Text = "Zrusit"
-$btnCancel.Size = New-Object System.Drawing.Size(80, 30)
-$btnCancel.Location = New-Object System.Drawing.Point(320, 265)
+$btnCancel.Size = New-Object System.Drawing.Size(90, 30)
+$btnCancel.Location = New-Object System.Drawing.Point(400, 325)
 $btnCancel.Add_Click({
     $form.Tag = "CANCEL"
     $form.Close()
 })
 $form.Controls.Add($btnCancel)
 
-# Zobrazit formular
 [void]$form.ShowDialog()
+
 
 if ($form.Tag -ne "OK") {
     Write-Host "Inventura byla zrusena uzivatelem."
@@ -135,4 +137,5 @@ try {
 catch {
     Write-Host "Chyba pri odesilani dat do Google Sheets." -ForegroundColor Red
     Write-Host $_.Exception.Message
+
 }
